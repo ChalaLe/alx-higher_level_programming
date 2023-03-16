8f03dcc72093ab0b5f10e4d62c27cb6194b56eb1
ChalaLe <clelisa28@gmail.com>
2023-03-15 19:25:44 -0700

    0x04. Python - More Data Structures project tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,8 +5,11 @@ def weight_average(my_list=[]):
     weighted_sum = 0
     total_weight = 0
 
-    weighted_sum = sum(score * weight for score, weight in my_list)
-    total_weight = sum(weight for _, weight in my_list)
+    
+    for tup in my_list:
+        weighted_sum  += tup[0] * tup[1]
+        total_weight += tup[1]
+
 
     weighted_average = weighted_sum / total_weight
     
