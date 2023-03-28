8097bb129f693130e412fdd3f3f6b4c74e9b97c5
ChalaLe <clelisa28@gmail.com>
2023-03-28 15:03:03 -0700

    python-classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self._Square__size = size
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def area(self):
+        return self._Square__size ** 2
