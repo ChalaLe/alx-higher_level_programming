8bfa4eabd72279c219827dd8c6b5fc106aa4500c
ChalaLe <clelisa28@gmail.com>
2023-03-29 07:58:15 -0700

    python-classes tasks

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
 
 
 class Square:
+    """Represenr square class"""
 
     def __init__(self, size=0):
+        """Initialize square"""
         self.size = size
 
     @property
@@ -12,6 +15,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """Set size of square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,22 +23,29 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return area of square"""
         return (self.__size * self.__size)
 
     def __eq__(self, other):
+        """Define == comparison to square"""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """Define != comparison to square"""
         return self.area() != other.area()
 
     def __lt__(self, other):
+        """Define < comparison to square"""
         return self.area() < other.area()
 
     def __le__(self, other):
+        """Define <= comparison square"""
         return self.area() <= other.area()
 
     def __gt__(self, other):
+        """Define > comparison to square"""
         return self.area() > other.area()
 
     def __ge__(self, other):
+        """Define >= comparison to square"""
         return self.area() >= other.area()
