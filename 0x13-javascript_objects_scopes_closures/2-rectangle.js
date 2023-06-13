6f68352f29e6a0ccb2caffcb28d99b33ec88f67c
ChalaLe <clelisa28@gmail.com>
2023-06-13 10:14:32 -0700

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // Return an empty object
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
